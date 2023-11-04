e7815c0c7967c539fb82cf9e6f197e0fbc22943b
qhouyee <quekhouyee@gmail.com>
2023-11-04 13:30:38 +0800

    dev-select-stations-ui: Update method documentation

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,13 +2,13 @@ import { DomTreeHelper } from './utils/dom_tree_helper';
 
 /**
  * This component acts as an entry point to render a web application.
- */
+*/
 export class App {
   private rootElement: HTMLElement;
 
   /**
-   * Standard constructor to set up the app.
-   */
+    * Standard constructor to set up the app.
+  */
   constructor() {
     // Set up a default app container
     this.rootElement = DomTreeHelper.createDiv("app");
@@ -17,7 +17,8 @@ export class App {
 
   /**
     * Render the content onto the web page.
-    */
+    * @returns {void}
+  */
   public render(): void {
     // Appends all created container to the document body
     document.body.appendChild(this.rootElement);
--- src/ts/utils/dom_tree_helper.ts
+++ src/ts/utils/dom_tree_helper.ts
@@ -1,22 +1,22 @@
 /**
  * This component provides methods for retrieving and modifying the Html Elements in a DOM tree.
- */
+*/
 export class DomTreeHelper {
   
   /**
-  * Create and return a new div element.
-  * @param {string} id - An optional id attribute to set for the div element.
-  * @returns The created div element.
+    * Create and return a new div element.
+    * @param {string} id - An optional id attribute to set for the div element.
+    * @returns {HTMLDivElement} The created div element.
   */
   static createDiv(id?: string): HTMLDivElement {
     return <HTMLDivElement>this.createHtmlElement("div", id);
   }
 
   /**
-  * Create and return a new HTML element of specified type.
-  * @param elementType - The type of HTML element to create.
-  * @param {string} id - An optional id attribute to set for the html element.
-  * @returns The created HTML element.
+    * Create and return a new HTML element of specified type.
+    * @param {string} elementType - The type of HTML element to create.
+    * @param {string} id - An optional id attribute to set for the html element.
+    * @returns {HTMLElement} The created HTML element.
   */
   static createHtmlElement(elementType: string, id?: string): HTMLElement {
     let htmlElement = document.createElement(elementType);
@@ -25,10 +25,10 @@ export class DomTreeHelper {
   }
 
   /**
-  * Fetches an HTML element by its ID from the document.
-  * @param id - The ID of the element to fetch.
-  * @returns The HTML element with the specified ID.
-  * @throws Error if the element is not found.
+    * Fetches an HTML element by its ID from the document.
+    * @param {string} id - The ID of the element to fetch.
+    * @returns {HTMLElement} The HTML element with the specified ID.
+    * @throws {Error} if the element is not found.
   */
   static getElementById(id: string): HTMLElement {
     let element = document.getElementById(id);
