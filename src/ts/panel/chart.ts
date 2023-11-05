15f10c21cc2a9bde767fa7cedf8d3d7c91431195
qhouyee <quekhouyee@gmail.com>
2023-11-05 15:02:18 +0800

    dev-add-chart-display: Added chart component

---
+++ src/ts/panel/chart.ts
@@ -0,0 +1,80 @@
+import Chart from 'chart.js/auto';
+
+import { DomTreeHelper } from '../utils/dom_tree_helper';
+
+/**
+ * This interface represents a reading for water level measures that is relevant to our application.
+*/
+export interface WaterLevelReading {
+  dateTime: string;
+  value: number;
+}
+
+/**
+ * This component sets up a panel containing the web app's functionality.
+*/
+export class ChartComponent {
+  private chartContainer: HTMLDivElement;
+  private chart: Chart;
+
+  /**
+   * Constructor.
+  */ constructor() {
+    // Set up the panel container
+    this.chartContainer = DomTreeHelper.createDiv("chart");
+    this.initChart()
+  }
+
+  /**
+   * Renders the component for usage.
+   * @returns {HTMLDivElement} the chart element.
+  */ public render(): HTMLDivElement {
+    return this.chartContainer;
+  }
+
+  /**
+   * Updates the chart based on data inputs.
+   * @param {WaterLevelReading[]} readings - An array of the water level readings returned from API. 
+   * @returns {void}.
+  */ public update(readings: WaterLevelReading[]): void {
+    this.chart.data.labels = readings.map(reading => reading.dateTime);
+    this.chart.data.datasets[0].data = readings.map(reading => reading.value);
+    this.chart.update();
+  }
+
+  /**
+   * Initialise a new empty chart.
+   * @returns {void}
+  */ private initChart(): void {
+    // Create a canvas for the chart and append it to the container
+    let chartElement: HTMLCanvasElement = <HTMLCanvasElement>DomTreeHelper.createHtmlElement("canvas", "readings");
+    this.chartContainer.appendChild(chartElement);
+    // Initialise an empty chart
+    this.chart = new Chart(
+      chartElement,
+      {
+        type: "line",
+        data: {
+          labels: [],
+          datasets: [
+            {
+              data: []
+            }
+          ]
+        },
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: "Time series of water level"
+            },
+            legend: {
+              display: false,
+            }
+          }
+        }
+      }
+    );
+  }
+}
+
